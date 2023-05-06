645dceabaaaafb1379a8fe4af0f8c707ba762bc5
harshal <harshalpawar013@gmail.com>
2023-05-06 23:47:22 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const app=express()
+require('./db/config')
+const user=require('./db/model.js')
+
+app.use(express.json())
+
+// sign up API
+app.post('/Register',async(req,res)=>{
+    let data= await user(req.body)
+    let result=await data.save()
+    // result=result.toObject();
+    // delete result.password
+    res.send(result)
+})
+app.listen(5000,console.log('server is running on port 5000'))
